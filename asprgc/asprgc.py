3fdfde6f982400ecbec5ae5bbf72c105ac208a44
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2015-05-21 14:08:06 +0200

    full compatibility python 3

--- asprgc/asprgc.py
+++ asprgc/asprgc.py
@@ -2,7 +2,7 @@
 """
 TOWRITE
 """
-from __future__   import print_function, absolute_import
+from __future__   import absolute_import, print_function
 from builtins     import input
 from future.utils import itervalues, iteritems
 from collections  import defaultdict
