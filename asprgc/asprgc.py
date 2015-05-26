64ef4210a7a1e307c78594c5c14e0b31f4a217b8
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2015-05-26 18:31:41 +0200

    one solver for each step

--- asprgc/asprgc.py
+++ asprgc/asprgc.py
@@ -10,6 +10,7 @@ from aspsolver    import ASPSolver
 from commons      import basename
 import converter  as converter_module
 import commons
+import gringo
 import atoms
 
 
@@ -41,17 +42,27 @@ def asprgc(iterations, graph_data, extracting, ccfinding, updating, remaining,
     logger.info('#### EXTRACT ####')
     logger.info('#################')
     # creat a solver that get all information about the graph
-    extractor = ASPSolver().use(graph, program_name='base').use(extract)
-    graph_atoms = extractor.first_solution().atoms()
+    extractor = gringo.Control()
+    extractor.load(graph_data)
+    extractor.ground([('base', [])])
+    extractor.load(extracting)
+    extractor.ground([(basename(extracting), [])])
+
+    model = commons.first_solution(extractor)
+    assert(model is not None)
+    graph_atoms = model.atoms()
     # get all CC, one by one
     atom_ccs = (cc.args()[0]  # args is a list of only one element (cc/1)
                 for cc in graph_atoms
                 if cc.name() == 'cc'
                )
     # save atoms as ASP-readable string
+    all_edges   = atoms.to_str(graph_atoms, names='ccedge')
     graph_atoms = atoms.to_str(graph_atoms)
+    del extractor
     # printings
     logger.debug('EXTRACTED: ' + graph_atoms + '\n')
+    logger.debug('CCEDGES  : ' + all_edges + '\n')
 
     # Find connected components
     logger.info('#################')
@@ -60,26 +71,33 @@ def asprgc(iterations, graph_data, extracting, ccfinding, updating, remaining,
     for cc in atom_ccs:
         # Solver creation
         logger.info('#### CC: ' + str(cc) + ' ' + str(cc.__class__))
-        solver = ASPSolver(commons.ASP_OPTIONS)
-        solver.read(graph_atoms)       # read all basical data
-        solver._prg.load(ccfinding)
 
         # main loop
         k = 0
+        previous_coverage = ''
+        model = None
         while True:
-            # FIND BEST CONCEPT
-            # release previous k, ground program and assign new k
-            solver.assign_external(name='step', args=[k], truth=False)
             k += 1
-            solver.ground(basename(ccfinding), [cc,k])
-            solver.assign_external(name='step', args=[k], truth=True)
+            # FIND BEST CONCEPT
+            # create new solver and ground all data
+            logger.info('\tINPUT: ' + '.\n\t'.join(_ for _ in previous_coverage.split('.') if '(2,' in _))
+            logger.info('\tINPUT: ' + '.\n\t'.join(_ for _ in previous_coverage.split('.') if 'ed(' in _))
+            logger.info('\tINPUT: ' + previous_coverage)
+            solver = gringo.Control(commons.ASP_OPTIONS)
+            solver.add('base', [], graph_atoms + previous_coverage)
+            solver.ground([('base', [])])
+            solver.load(ccfinding)
+            solver.ground([(basename(ccfinding), [cc,k])])
 
             # solving
-            model = solver.first_solution()
+            model = commons.first_solution(solver)
+            # treatment of the model
             if model is None:
                 print('No model found by bcfinder')
                 break
-            logger.info('\tOUTPUT: ' + str(atoms.to_str(model.atoms(), separator='.\n\t')))
+            logger.info('\tOUTPUT: ' + atoms.to_str(
+                model.atoms(), separator='.\n\t'
+            ))
             logger.info('\tOUTPUT: ' + str(atoms.count(model.atoms())))
 
             logger.info('POWERNODES:\n\t' + atoms.prettified(
@@ -88,6 +106,7 @@ def asprgc(iterations, graph_data, extracting, ccfinding, updating, remaining,
                 joiner='\n\t',
                 sort=True
             ))
+            previous_coverage += atoms.to_str(model.atoms(), names=('covered', 'block'))
 
             # give new powernodes to converter
             converter.convert(model.atoms(), separator=', ')
@@ -95,49 +114,52 @@ def asprgc(iterations, graph_data, extracting, ccfinding, updating, remaining,
             if interactive:
                 input('Next ?')  # my name is spam
 
-        # release cc value
-        solver.release_external(fun=cc)
 
 
 
-    logger.info('#################')
-    logger.info('## REMAIN DATA ##')
-    logger.info('#################')
-    # get atoms necessary for remaining edges solving
-    input_atoms_names = ('ccedge', 'covered')
-    input_atoms = [a for a in model.atoms() if a.name() in input_atoms_names]
+        logger.info('#################')
+        logger.info('## REMAIN DATA ##')
+        logger.info('#################')
+        # Creat solver and collect remaining edges
+        # logger.debug("INPUT REMAIN: " + all_edges + previous_coverage)
+        remain_collector = gringo.Control()
+        remain_collector.load(remaining)
+        remain_collector.add('base', [], all_edges + previous_coverage)
+        remain_collector.ground([
+            (basename(remaining), []),
+            ('base', []),
+        ])
+        remain_edges = commons.first_solution(remain_collector)
+        # logger.debug("OUTPUT REMAIN: " + str(remain_edges))
 
-    # Creat solver and collect remaining edges
-    remain_collector = ASPSolver().use(remain)
-    remain_collector.read(atoms.to_str(input_atoms))
-    remain_edges = remain_collector.first_solution()
-
-    # Output
-    if remain_edges is None or len(remain_edges.atoms()) == 0:
-        logger.info('No remaining edge')
-    else:
-        converter.convert(remain_edges.atoms())
+        # Output
+        if remain_edges is None or len(remain_edges.atoms()) == 0:
+            logger.info('No remaining edge')
+        else:
+            logger.info(str(len(remain_edges.atoms())) + ' remaining edge(s)')
+            converter.convert(remain_edges.atoms())
 
 
 
-    logger.info('#################')
-    logger.info('#### RESULTS ####')
-    logger.info('#################')
-    # write output in file
-    output.write(converter.finalized())
-    output.close()
-    logger.debug('FINAL DATA SAVED IN FILE ' + output_file + '.' + output_format)
+        logger.info('#################')
+        logger.info('#### RESULTS ####')
+        logger.info('#################')
+        # write output in file
+        output.write(converter.finalized())
+        output.close()
+        logger.debug('FINAL DATA SAVED IN FILE ' + output_file + '.' + output_format)
 
-    # print results
-    results_names = ('powernode')
-    logger.info('\n\t' + atoms.prettified(all_atoms,
-                                          results_only=True,
-                                          joiner='\n\t',
-                                          sort=True)
-    )
-    for to_find in ('powernode', 'edgecover'):
-        logger.info(to_find + ' found: \t' + str(to_find in str(all_atoms)))
+        # print results
+        # results_names = ('powernode',)
+        # logger.info('\n\t' + atoms.prettified(all_atoms,
+                                              # results_only=True,
+                                              # joiner='\n\t',
+                                              # sort=True)
+        # )
+        # for to_find in ('powernode', 'edgecover'):
+            # logger.info(to_find + ' found: \t' + str(to_find in str(all_atoms)))
 
+    logger.info("All cc has been performed")
 
     # return str(graph)
 
