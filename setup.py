77762590d9e1ff6b084b8f87b8f86e1891913e11
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2015-06-17 10:23:29 +0200

    add requirements information for installation
    note that python-gringo is given as requirement, but it is not
    installable through pip since it is not uploaded on pypi.

--- setup.py
+++ setup.py
@@ -9,7 +9,14 @@
 #########################
 from setuptools  import setup, find_packages
 from powergrasp.info import __version__, __name__
+from pip.req import parse_requirements
 
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(<requirements_path>)
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 
 
 #########################
@@ -24,6 +31,7 @@ setup(
         __name__ : ['README.mkd', 'LICENSE.txt']
     },
     include_package_data = True,
+    install_requires=reqs,
 
     author = "lucas bourneuf",
     author_email = "lucas.bourneuf@openmailbox.org",
