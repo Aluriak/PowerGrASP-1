38b830160bcc32d5afb152badfc131dba28e2285
Aluriak <lucas.bourneuf@laposte.net>
2015-10-23 09:49:30 +0000

    includes logs, data and tests in the packaging

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@
 from setuptools  import setup, find_packages
 from powergrasp.info import __version__, __name__
 from pip.req import parse_requirements
+from pip.download import PipSession
 
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', session=False)
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
+# reqs = 
 
 
 #########################
@@ -26,7 +28,10 @@ setup(
     py_modules = ['info'],
     packages = find_packages(exclude=['powergrasp/']),
     package_data = {
-        __name__ : ['README.mkd', 'LICENSE.txt']
+        __name__ : ['README.mkd', 'LICENSE.txt', 'requirements.txt',
+                    'optional-requirements.txt', 'Makefile', 'licence',
+                    'logs/powergrasp.log', 'tests/*.lp', 'tests/*.gml',
+                    'data/*.csv']
     },
     include_package_data = True,
     install_requires=reqs,
