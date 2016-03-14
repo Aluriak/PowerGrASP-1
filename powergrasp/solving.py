d3a900bd4978d59edf65b923376e0c48a5d2e194
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-03-14 09:11:31 +0100

    solving: use pyasp for atom spliting instead of locally defined functions

--- powergrasp/solving.py
+++ powergrasp/solving.py
@@ -15,10 +15,24 @@ from pyasp import asp
 LOGGER = commons.logger()
 
 
-def model_from(base_atoms, aspfiles, aspargs={},
-               gringo_options=commons.ASP_GRINGO_OPTIONS,
-               clasp_options=commons.ASP_CLASP_OPTIONS):
-    """Compute a model from ASP source code in aspfiles, with aspargs
+class Atoms(asp.TermSet):
+    """This class is a patching of TermSet, and is returned in place of it
+    by the functions of this module."""
+
+    def get(self, atom_names):
+        if isinstance(atom_names, str):
+            return (atom for atom in self if atom.predicate == atom_names)
+        else:
+            return (atom for atom in self if atom.predicate in atom_names)
+
+    def get_first(self, atom_name):
+        return next(atom for atom in self if atom.predicate == atom_name)
+
+
+def all_models_from(base_atoms, aspfiles, aspargs={},
+                    gringo_options=commons.ASP_GRINGO_OPTIONS,
+                    clasp_options=commons.ASP_CLASP_OPTIONS):
+    """Compute all models from ASP source code in aspfiles, with aspargs
     given as grounding arguments and base_atoms given as input atoms.
 
     base_atoms -- string, ASP-readable atoms
@@ -26,6 +40,7 @@ def model_from(base_atoms, aspfiles, aspargs={},
     aspargs -- dict of constant:value that will be set as constants in aspfiles
     gringo_options -- string of command-line options given to gringo
     clasp_options -- string of command-line options given to clasp
+    yield -- Atoms instance, containing atoms produced by solving
 
     """
     # use the right basename and use list of aspfiles in all cases
@@ -47,20 +62,34 @@ def model_from(base_atoms, aspfiles, aspargs={},
                               clasp_options=clasp_options)
     # print('SOLVING:', aspfiles, constants)
     # print('INPUT:', base_atoms.__class__, base_atoms)
-    answers = solver.run(aspfiles, additionalProgramText=base_atoms)
+    answers = solver.run(aspfiles, additionalProgramText=base_atoms,
+                         collapseTerms=True, collapseAtoms=False)
     # if len(answers) > 0:
         # for idx, answer in enumerate(answers):
             # print('ANSWER ' + str(idx) + ':', answer)
             # print('ATOM(S):', atoms.count(answer))
     # else:
         # print('NO MODEL FOUND !')
+    yield from (Atoms(answer) for answer in answers)
 
-    # return the last solution (which is the best), or None if no solution
+
+def model_from(base_atoms, aspfiles, aspargs={},
+               gringo_options=commons.ASP_GRINGO_OPTIONS,
+               clasp_options=commons.ASP_CLASP_OPTIONS):
+    """Compute the last model from ASP source code in aspfiles, with aspargs
+    given as grounding arguments and base_atoms given as input atoms.
+
+    base_atoms -- string, ASP-readable atoms
+    aspfiles -- (list of) filename, contains the ASP source code
+    aspargs -- dict of constant:value that will be set as constants in aspfiles
+    gringo_options -- string of command-line options given to gringo
+    clasp_options -- string of command-line options given to clasp
+    return -- an Atoms instance containing atoms of the last produced model
+
+    """
+    answers = all_models_from(base_atoms, aspfiles, aspargs,
+                              gringo_options, clasp_options)
     try:
-        last_solution = deque(answers, maxlen=1)[0]
-        # print('LAST_SOLUTION: ', len(last_solution), ' "', last_solution, '"', sep='')
-        # a solution is valid if at least one atom is returned
-        return last_solution if len(last_solution) > 0 else None
-    except IndexError:
-        # no valid model
+        return deque(answers, maxlen=1)[0]
+    except IndexError:  # no valid model
         return None
