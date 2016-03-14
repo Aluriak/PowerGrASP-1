afd2ab4ce14b27ee546379b53f81b1c7376b622f
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-03-14 19:00:07 +0100

    profiling: fix last example filepath + output in standard place

--- profiling.py
+++ profiling.py
@@ -10,11 +10,11 @@ FILE = DIR_TESTS + 'structural_binding.lp'  # sbind
 FILE = DIR_TESTS + 'big_biclique.lp'
 FILE = DIR_TESTS + 'big_biclique_merged.lp'
 FILE = DIR_TESTS + 'double_biclique.lp'  # ddiam
-# FILE = './rrel_chr1_chr38.lp'  # chr
+# FILE = DIR_TESTS + './rrel_chr1_chr38.lp'  # chr
 
 p.compress(
     graph_data=FILE,
-    output_file='./output.bbl',
+    output_file='./powergrasp/data/output.bbl',
     loglevel='warning',
 )
 print('Compression finished !')
