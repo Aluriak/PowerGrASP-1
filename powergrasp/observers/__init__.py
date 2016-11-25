e62f5a1bfd0ac375dccbc9ffe7a134df30d95e4d
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-11-25 11:28:20 +0100

    observers: implementation of the ObserverBatch, an interface to a bunch of observers

--- powergrasp/observers/__init__.py
+++ powergrasp/observers/__init__.py
@@ -1,4 +1,4 @@
-from .observer import Signals, Priorities, CompressionObserver
+from .observer import Signals, Priorities, CompressionObserver, ObserverBatch
 from .counters import ConnectedComponentsCounter, ObjectCounter
 from .output import InteractiveCompression, LatticeDrawer, OutputWriter, TimeComparator
 from .time import TimeCounter, NullTimeCounter
--- powergrasp/observers/observer.py
+++ powergrasp/observers/observer.py
@@ -75,3 +75,66 @@ class CompressionObserver:
     @property
     def priority(self):
         return Priorities.Medium
+
+
+class ObserverBatch:
+    """Container of observers, providing an API for observed objects.
+
+    Proxy to all contained Observers, having a reference to the configuration
+    that have been used to create the 
+
+    It also infer some signals, like 'Compression started',
+    based on the received signals.
+
+    """
+    def __init__(self, observers:iter, configuration):
+        self._observers = tuple(observers)
+        self._configuration = configuration
+        self.sort_observers()
+
+    @property
+    def observers(self):
+        return self._observers
+
+    @property
+    def configuration(self):
+        return self._configuration
+
+    def __iter__(self):
+        return iter(self.observers)
+
+    def signal(self, *args, **kwargs):
+        for observer in self.observers:
+            observer.update(*args, **kwargs)
+
+    def __iadd__(self, othr):
+        """Alias for extend method"""
+        self.extend(othr)
+        return self
+
+    def __add__(self, others):
+        """Return a new ObserverBatch instance, created using extend method"""
+        ret = ObserverBatch(self.observers, self.configuration)
+        ret.extend(others)
+        return ret
+
+    def extend(self, others):
+        """Return a new batch containing observers in self and
+        observers found in othr.
+
+        others -- a list of CompressionObservers, or an ObserverBatch
+
+        """
+        whole = self._observers
+        if isinstance(others, ObserverBatch):
+            others = tuple(others.observers)
+        for obs in others:
+            if not isinstance(obs, CompressionObserver):
+                raise ValueError("Object {} is not a CompressionObserver "
+                                 "instance, as needed by ObserverBatch.".format(obs))
+        self._observers.extend(others)
+        self.sort_observers()
+
+    def sort_observers(self):
+        """Sort internals observers in order to preserve their priority order"""
+        self._observers = sorted(self._observers, key=lambda o: o.priority.value, reverse=True)
