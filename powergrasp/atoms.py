66030d1f957854f93ceeea341bebd0e5017e7fc4
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-11-28 18:15:07 +0100

    atoms: remove useless function

--- powergrasp/atoms.py
+++ powergrasp/atoms.py
@@ -215,69 +215,6 @@ def first_arg(atom):
         return None
 
 
-def prettified(atoms, names=None, sizes=None,
-               joiner='\n', results_only=False, sort=False, hashs=False):
-    """Return a human readable string version or
-      string generator of given atoms.
-
-    if names is given, only atoms with given name will be used.
-    if results_only is True, only some atoms will be printed.
-      (defined by RESULTS_PREDICATS)
-    if sizes is provided, atoms with args number not in sizes
-      will not be printed.
-
-    if sort is True, a non-lazy treatment will be applied to all data,
-      and the atoms will be returned in sorted order.
-
-    if hash is True, each atom will be printed with its hash.
-
-    atoms must be an iterable of gringo.Fun instances.
-
-    """
-    atoms, hashs_values = itertools.tee(atoms)
-    atoms = ((atom.predicate, atom.arguments) for atom in atoms)
-    if hashs:
-        hashs_values = (a.__hash__() for a in hashs_values)
-    # filter results
-    if results_only:
-        source = ((n,a)
-                  for n,a in atoms
-                  if n in RESULTS_PREDICATS
-                 )
-    else:
-        source = (_ for _ in atoms)
-    # filter names
-    if names is not None:
-        source = ((name,args)
-                  for name,args in source
-                  if name in names
-                 )
-    # filter sizes
-    if sizes is not None:
-        source = (
-                  (name,args)
-                  for name, argset in source
-                  if len(args) in sizes
-                 )
-    # get final text generator
-    source = (
-        name+'('+','.join(str(_) for _ in args)+').'
-        for name, args in source
-    )
-    # hashs
-    if hashs:
-        source = (
-            str(hash) + ' ' + name
-            for name, hash in zip(source, hashs_values)
-        )
-    # sorting
-    if sort:
-        source = sorted(tuple(source))
-
-    # joining if possible
-    return source if joiner is None else joiner.join(source)
-
-
 def to_str(atoms, names=None, separator='.'):
     """Return string that is equivalent and ASP-valid from
     given atoms.
