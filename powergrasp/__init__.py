1c397f1d10d404a1524bd56a71c12f5b5f27e9a4
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-12-12 18:32:28 +0100

    init: provide Configuration and its alias at top package level

--- powergrasp/__init__.py
+++ powergrasp/__init__.py
@@ -1,4 +1,6 @@
 from powergrasp import info
 from powergrasp import motif
 from powergrasp.graph import Graph
+from powergrasp.config import Configuration
+Config = Configuration  # simple alias
 from powergrasp.plotting import plots
