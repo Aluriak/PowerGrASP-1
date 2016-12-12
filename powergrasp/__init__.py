bcf3e7d9fd5f56027f6a0638f15e02bc42180617
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-12-12 19:27:04 +0100

    init: expose more submodules at top level API

--- powergrasp/__init__.py
+++ powergrasp/__init__.py
@@ -1,5 +1,7 @@
 from powergrasp import info
 from powergrasp import motif
+from powergrasp import recipes
+from powergrasp import observers
 from powergrasp.graph import Graph
 from powergrasp.config import Configuration
 Config = Configuration  # simple alias
