247fef4944aadc6917cd4f3ad915701d8df36a5f
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-12-14 15:34:47 +0100

    init: package doc for users

--- powergrasp/__init__.py
+++ powergrasp/__init__.py
@@ -1,3 +1,18 @@
+"""PowerGrASP is a package implementing graph compression methods.
+
+You can found the official repository at http://powergrasp.bourneuf.net
+
+High level API is described in the recipes module,
+where compression algorithms are implemented.
+
+See also the config module where the Configuration object, allowing one to
+precisely define the compression parameters, is implemented.
+
+The CLI also provide a good starting point, allowing one to play directly
+with graphs without having to code anything.
+
+"""
+
 from powergrasp import info
 from powergrasp import motif
 from powergrasp import recipes
