f6e3d4cb745a26a56bc5a826b2dcaa9c5dd4d56a
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-01-05 17:54:22 +0100

    addon: KnHPF implementation

--- powergrasp/motif/addon.py
+++ powergrasp/motif/addon.py
@@ -29,6 +29,49 @@ Addon = namedtuple('Addon', 'files add_atoms add_constants')
 Addon.__new__.__defaults__ = (), lambda m: m, lambda _: {}
 
 
+def addon_knodes_degree(model:AtomsModel, k:int=2):
+    """Add maximal degrees atoms to input model.
+
+    A maximal node is a node that share the maximal number of common neigbors
+    with k other nodes, and have the greater degree among them.
+    With k=1, this function is equivalent
+    to addon_degree(include_max_node_degrees=True).
+    With k=2, this function also mark as maximal priority the node that share
+    N common neighbors with another node, with N the maximal size of a biclique
+    implying two elements in one set. Of the two nodes, only those with maximal
+    degree is kept. (both in case of equality)
+
+    """
+    # get the complete graph as a dict {pred: {succs}}
+    graph = defaultdict(set)
+    for na, nb in model.get_args('edge'):
+        graph[na].add(nb)
+        graph[nb].add(na)
+    graph_nodes = tuple(graph.keys())
+    max_nodes = set()  # all nodes marked as maximal priority
+    for node_per_set in range(1, k+1):
+        k_uplets = list()  # all k-uplets that have the maximal common neighbor
+        maximal_degree = 0
+        for nodes in itertools.combinations(graph_nodes, node_per_set):
+            succs = set(graph_nodes)
+            for node in nodes:
+                succs &= graph[node]
+            if len(succs) == maximal_degree:  # found other k nodes with maximal number of common neighbor
+                k_uplets.append(set(nodes))
+            elif len(succs) > maximal_degree:  # found higher number of common neighbor
+                k_uplets = [set(nodes)]
+                maximal_degree = len(succs)
+        # Compute the maximal degree for all nodes in k-uplet
+        # Add nodes of maximal degree to max_nodes for each node in 
+        for k_uplet in k_uplets:
+            degrees = {node: len(graph[node]) for node in k_uplet}
+            maximal_degree = max(degrees.values(), default=-1)
+            max_nodes |= {node for node, degree in degrees.items()
+                          if degree == maximal_degree}
+    model.set_args('max_priority', ((node,) for node in max_nodes))
+    return model
+
+
 def addon_degree(model:AtomsModel, include_node_degrees:bool=False,
                  include_max_node_degrees:bool=False):
     """Add degrees and maximal degrees atoms to input model"""
@@ -59,4 +102,9 @@ ByDegree = Addon(
 ByFuzzyDegree = Addon(
     [commons.ASP_SRC_FUZZY_PRIORITY],
     partial(addon_degree, include_node_degrees=True),
+)
+
+ByKNodesDegree = Addon(
+    [commons.ASP_SRC_PRIORITY],
+    partial(addon_knodes_degree, k=2),
 )
